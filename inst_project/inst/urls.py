508ff81916c2b2ae9ccd91b17f831dfabdb7b781
Chanita Air Anuwong <chanita@odds.team>
2019-08-22 13:56:44 +0700

    Add basic views

--- inst_project/inst/urls.py
+++ inst_project/inst/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from home.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('', home, name='home')
 ]
